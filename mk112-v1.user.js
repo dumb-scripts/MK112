824eb0af99f73639a9c72610a4d80bbbb381d53e
Martin Diphoorn <martin@diphoorn.com>
2020-05-11 14:22:07 +0200

    First version

---
+++ mk112-v1.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         MK-112
+// @namespace    http://meldkamersspel.com/
+// @version      0.0.1
+// @description  Game enriching
+// @author       Rumpie Dumb
+// @match        https://www.meldkamerspel.com/*
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
+// @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @updateURL    https://github.com/dumb-scripts/MK112/raw/master/mk112-v1.user.js
+// @grant        none
+// @run          document-start
+// ==/UserScript==
+
+this.$ = this.jQuery = jQuery.noConflict(true);
+
+console.log('starter');

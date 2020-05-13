518ca853e2f3700bb33ab17c77baee02ad80cde3
Martin Diphoorn <martin@diphoorn.com>
2020-05-13 20:53:33 +0200

    Add quick team alert buttons

--- mk112-v1.user.js
+++ mk112-v1.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         MK-112
 // @namespace    http://meldkamersspel.com/
-// @version      0.0.4
+// @version      0.0.5
 // @description  Game enriching
 // @author       Dumb Scripts
 // @match        https://www.meldkamerspel.com/*
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // @require      https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @require      http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.2/moment.min.js
 // @updateURL    https://raw.githubusercontent.com/dumb-scripts/MK112/master/mk112-v1.user.js
 // @grant        none
 // @run          document-start
@@ -113,9 +114,51 @@ var missionTypes = JSON.parse(localStorage.getItem('mk112-mission-types'));
             var missionTypeId = +parts[2];
 
             var missionType = missionTypes.find(value => value.id === missionTypeId);
+            const missionCredits = (missionType && missionType.credits) ? missionType.credits : '?';
+
             if (missionType && missionType.credits) {
                 var elTitle = document.querySelector('#missionH1');
-                elTitle.innerHTML = elTitle.innerHTML + ' <b>(' + missionType.credits + ' credits)</b>';
+                elTitle.innerHTML = elTitle.innerHTML + ' <b>(' + missionCredits + ' credits)</b>';
+            }
+
+            var inputReply = document.querySelector('#mission_reply_content');
+            if (inputReply) {
+                const btnGroup = document.createElement('div');
+                btnGroup.classList.add('btn-group');
+
+                const buttonFunc = (lastVehicle) => {
+                    const date = new Date();
+                    date.setHours( date.getHours() + 2 );
+                    var timeText = moment(date).format('HH:mm');
+                    inputReply.value = `${missionCredits} Credits, ${lastVehicle} na ${timeText}`;
+                    return false;
+                }
+
+                const btnOvD = document.createElement("button");
+                btnOvD.innerHTML = 'OvD';
+                btnOvD.classList.add('btn', 'btn-default');
+                btnOvD.type = 'button';
+                btnOvD.onclick = () => buttonFunc('OvD');
+
+                const btnHOD = document.createElement("button");
+                btnHOD.innerHTML = 'HOD';
+                btnHOD.type = 'button';
+                btnHOD.classList.add('btn', 'btn-default');
+                btnHOD.onclick = () => buttonFunc('HOD');
+
+                const btnCO = document.createElement("button");
+                btnCO.innerHTML = 'CO';
+                btnCO.type = 'button';
+                btnCO.classList.add('btn', 'btn-default');
+                btnCO.onclick = () => buttonFunc('CO');
+
+                btnGroup.appendChild(btnOvD);
+                btnGroup.appendChild(btnHOD);
+                btnGroup.appendChild(btnCO);
+                inputReply.parentNode.before(btnGroup);
+
+            } else {
+              console.log('inputReply not found');
             }
         });
     }
